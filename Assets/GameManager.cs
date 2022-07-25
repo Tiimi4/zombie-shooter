c761e8062facccb42954f8413e5f7694fd45af67
Apluri <henri.hietala95@gmail.com>
2022-07-25 11:41:54 +0300

    UI (#8)
    * Add ui text elements for enemieskilled, time survived, hp and ammoleft
    
    * Update ammotext values

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
     private float _timeSurvived = 0f;
+    private int _timeSurvivedInt = 0;
     public float difficultyIncreaseInterval;
     private float _timeSincePreviousDifficultyIncrease = 0f;
     private float _timeSincePreviusEnemySpawn = 0f;
@@ -19,17 +21,22 @@ public class GameManager : MonoBehaviour
 
     public GameObject enemyPrefab;
 
+    private int _enemiesKilled = 0;
+
+    private TextMeshProUGUI _enemiesKilledText;
+    private TextMeshProUGUI _timeSurvivedText;
     public Transform[] spawnPoints;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _enemiesKilledText = GameObject.Find("enemiesKilled").GetComponent<TextMeshProUGUI>();
+        _timeSurvivedText = GameObject.Find("TimeSurvived").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _timeSurvived += Time.deltaTime;
+        UpdateTimeSurvived();
         _timeSincePreviusEnemySpawn += Time.deltaTime;
         _timeSincePreviousDifficultyIncrease += Time.deltaTime;
         
@@ -52,6 +59,15 @@ public class GameManager : MonoBehaviour
 
     }
 
+    private void UpdateTimeSurvived()
+    {
+        _timeSurvived += Time.deltaTime;
+        if (_timeSurvived - _timeSurvivedInt > 1)
+        {
+            _timeSurvivedInt += 1;
+            _timeSurvivedText.text = "Time survived " + _timeSurvivedInt + "s";
+        }
+    }
     private void SpawnEnemy()
     {
         _spawnAmount = Random.Range(_minSpawnAmount, _maxSpawnAmount);
@@ -62,6 +78,13 @@ public class GameManager : MonoBehaviour
             _timeSincePreviusEnemySpawn = 0f;
         }
       
+    }
+
+    public void AddEnemyKilled()
+    {
+        _enemiesKilled += 1;
+        _enemiesKilledText.text = "Enemies killed " + _enemiesKilled;
+        
     }
 }
 
